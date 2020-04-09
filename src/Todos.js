775c193a83600cd9eb00588b05c9f517a16dd1f5
yhorie <yuk.horie@gmail.com>
2020-04-09 10:58:26 +0900

    Improve Todos component

--- src/Todos.js
+++ src/Todos.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Todo from './Todo'
 
 class Todos extends React.Component {
+  static defaultProps = {
+    todos: [],
+  }
   render() {
-    return (
-      <ul>
-        <Todo />
-      </ul>
-    )
+    const listItems = this.props.todos.map((todo) => (
+      <Todo key={todo.id} text={todo.text} />
+    ))
+    return <ul>{listItems}</ul>
   }
 }
 
--- src/Todos.test.js
+++ src/Todos.test.js
@@ -1,36 +1,26 @@
-import React from "react";
-import { render, unmountComponentAtNode } from "react-dom";
-import { act } from "react-dom/test-utils";
+import React from 'react'
+//import jest from 'jest-mock'
+import { shallow } from 'enzyme'
 
-import Todos from "./Todo";
+import Todos from './Todos'
+describe('<Todos>', () => {
+  it('should output a ul', () => {
+    const wrapper = shallow(<Todos />)
+    expect(wrapper.find('ul')).toHaveLength(1)
+  })
+  it('accepts a todos prop', () => {
+    const todos = [
+      { id: 1, text: 'todo1' },
+      { id: 2, text: 'todo2' },
+    ]
+    const wrapper = shallow(<Todos todos={todos} />)
+    expect(wrapper.find('Todo')).toHaveLength(2)
 
-let container = null;
-beforeEach(() => {
-  // setup a DOM element as a render target
-  container = document.createElement("div");
-  document.body.appendChild(container);
-});
+    expect(wrapper.find('Todo').get(0).key).toEqual('1')
+    expect(wrapper.find('Todo').get(0).props.text).toEqual('todo1')
 
-afterEach(() => {
-  // cleanup on exiting
-  unmountComponentAtNode(container);
-  container.remove();
-  container = null;
-});
-
-it("renders with or without a name", () => {
-  act(() => {
-    render(<Todos text="todo"/>, container);
-  });
-  expect(container.textContent).toBe("todo");
-
-  // act(() => {
-  //   render(<Hello name="Jenny" />, container);
-  // });
-  // expect(container.textContent).toBe("Hello, Jenny!");
-
-  // act(() => {
-  //   render(<Hello name="Margaret" />, container);
-  // });
-  // expect(container.textContent).toBe("Hello, Margaret!");
-});
+    expect(wrapper.find('Todo').get(1).key).toEqual('2')
+    expect(wrapper.find('Todo').get(1).props.text).toEqual('todo2')
+  })
+  test.todo('accepts a todos prop')
+})
