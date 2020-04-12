2cd84a6383e87456b575df8fe0da6af25befdf01
yhorie <yuk.horie@gmail.com>
2020-04-12 14:18:06 +0900

    Add NewTodo

---
+++ src/NewTodo.test.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import jest from 'jest-mock'
+import NewTodo from './NewTodo'
+import { shallow } from 'enzyme'
+
+describe('<NewTodo>', () => {
+  it('should output a input', () => {
+    const wrapper = shallow(<NewTodo />)
+    expect(wrapper.find('input')).toHaveLength(1)
+  })
+
+  it('accepts a onDoubleClick prop', () => {
+    const mockChange = jest.fn()
+    const wrapper = shallow(<NewTodo onChange={mockChange} />)
+    wrapper.find('input').simulate('change')
+    expect(mockChange).toHaveBeenCalled()
+  })
+})
