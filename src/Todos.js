9ac170bf33f00580155c7d86b2a2a0005307e938
yhorie <yuk.horie@gmail.com>
2020-04-28 02:51:27 +0900

    Modify Todos component tree

--- src/Todos.js
+++ src/Todos.js
@@ -1,22 +1,8 @@
 import React from 'react'
-import Todo from './Todo'
 
 class Todos extends React.Component {
-  static defaultProps = {
-    todos: [],
-  }
   render() {
-    const listItems = this.props.todos.map((todo, index) => (
-      <Todo
-        key={index}
-        text={todo.text}
-        isEdit={todo.isEdit}
-        isDone={todo.isDone}
-        onDoubleClick={() => this.props.onDoubleClick(index)}
-        onClick={() => this.props.onClick(index)}
-      />
-    ))
-    return <ul>{listItems}</ul>
+    return <ul>{this.props.children}</ul>
   }
 }
 
